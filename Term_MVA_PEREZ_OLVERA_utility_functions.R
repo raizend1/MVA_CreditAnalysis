cad2b11be5d0665fab41f68cadcaed4e4f9f096e
raizend1 <pacogppl@hotmail.com>
2017-06-03 13:52:55 +0200

    Stratified sampling added

--- Term_MVA_PEREZ_OLVERA_utility_functions.R
+++ Term_MVA_PEREZ_OLVERA_utility_functions.R
@@ -222,6 +222,91 @@ mosthighlycorrelated <- function(mydataframe,numtoreport)
   head(fm[order(abs(fm$Correlation),decreasing=TRUE),],n=numtoreport)
 }
 
+#' @title Creates stratified sample
+#'
+#' @description
+#' Function to create stratified sampling. Credits to mrdwab from https://gist.github.com/mrdwab/6424112#file-stratified-r
+#' @param df: The input data.frame
+#' @param group: A character vector of the column or columns that make up the "strata".
+#' @param size: The desired sample size. 
+#' If size is a value less than 1, a proportionate sample is taken from each stratum.
+#' If size is a single integer of 1 or more, that number of samples is taken from each stratum.
+#' If size is a vector of integers, the specified number of samples is taken for each stratum. 
+#' It is recommended that you use a named vector. For example, if you have two strata, "A" and "B", 
+#' and you wanted 5 samples from "A" and 10 from "B", you would enter size = c(A = 5, B = 10).
+#' @param select: This allows you to subset the groups in the sampling process. This is a list. 
+#' For instance, if your group variable was "Group", and it contained three strata, "A", "B", and "C", 
+#' but you only wanted to sample from "A" and "C", you can use select = list(Group = c("A", "C")).
+#' @param replace: Boolean. For sampling with replacement.
+#' 
+#' @return log transformation for all the values
+#' 
+#' @examples
+
+# function to create stratified sampling. Credits to mrdwab from https://gist.github.com/mrdwab/6424112#file-stratified-r
+stratified <- function(df, group, size, select = NULL, 
+                       replace = FALSE, bothSets = FALSE) {
+  if (is.null(select)) {
+    df <- df
+  } else {
+    if (is.null(names(select))) stop("'select' must be a named list")
+    if (!all(names(select) %in% names(df)))
+      stop("Please verify your 'select' argument")
+    temp <- sapply(names(select),
+                   function(x) df[[x]] %in% select[[x]])
+    df <- df[rowSums(temp) == length(select), ]
+  }
+  df.interaction <- interaction(df[group], drop = TRUE)
+  df.table <- table(df.interaction)
+  df.split <- split(df, df.interaction)
+  if (length(size) > 1) {
+    if (length(size) != length(df.split))
+      stop("Number of groups is ", length(df.split),
+           " but number of sizes supplied is ", length(size))
+    if (is.null(names(size))) {
+      n <- setNames(size, names(df.split))
+      message(sQuote("size"), " vector entered as:\n\nsize = structure(c(",
+              paste(n, collapse = ", "), "),\n.Names = c(",
+              paste(shQuote(names(n)), collapse = ", "), ")) \n\n")
+    } else {
+      ifelse(all(names(size) %in% names(df.split)),
+             n <- size[names(df.split)],
+             stop("Named vector supplied with names ",
+                  paste(names(size), collapse = ", "),
+                  "\n but the names for the group levels are ",
+                  paste(names(df.split), collapse = ", ")))
+    }
+  } else if (size < 1) {
+    n <- round(df.table * size, digits = 0)
+  } else if (size >= 1) {
+    if (all(df.table >= size) || isTRUE(replace)) {
+      n <- setNames(rep(size, length.out = length(df.split)),
+                    names(df.split))
+    } else {
+      message(
+        "Some groups\n---",
+        paste(names(df.table[df.table < size]), collapse = ", "),
+        "---\ncontain fewer observations",
+        " than desired number of samples.\n",
+        "All observations have been returned from those groups.")
+      n <- c(sapply(df.table[df.table >= size], function(x) x = size),
+             df.table[df.table < size])
+    }
+  }
+  temp <- lapply(
+    names(df.split),
+    function(x) df.split[[x]][sample(df.table[x],
+                                     n[x], replace = replace), ])
+  set1 <- do.call("rbind", temp)
+  
+  if (isTRUE(bothSets)) {
+    set2 <- df[!rownames(df) %in% rownames(set1), ]
+    list(SET1 = set1, SET2 = set2)
+  } else {
+    set1
+  }
+}
+
 ####################ORIGINAL FUNCTIONS########################################
 # Original plotting·
 # ggplot(credit, aes(x = 0, y = LIMIT_BAL)) +
@@ -256,4 +341,51 @@ mosthighlycorrelated <- function(mydataframe,numtoreport)
 #       ggtitle(colnames(credit[i]))
 #     print(c)
 #   }
+# }
+
+# pca <-prcomp(credit.continuos)
+# clust <- kmeans(credit.continuos[,-15],3)
+# #credit.continuos$cluster <- as.factor(clust$cluster)
+# 
+# library(pca3d)
+# pca3d(credit.PCA,as.factor(clust$cluster))
+# 
+# library(rgl)
+# 
+# text3d(credit.PCA$ind$coord[,1:3],texts=rownames(credit))
+# text3d(credit.PCA$ind$coord[,1:3], texts=rownames(credit), col="red")
+# #code to plot from http://planspace.org/2013/02/03/pca-3d-visualization-and-clustering-in-r/
+# coords <- NULL
+# for (i in 1:nrow(credit.PCA$ind$coord)) {
+#   coords <- rbind(coords, rbind(c(0,0,0),credit.PCA$ind$coord[i,1:3]))
+# }
+# lines3d(coords, col="red", lwd=4)
+# 
+# # from previous sessions http://stackoverflow.com/questions/24282143/pca-multiplot-in-r
+# plotPCA <- function(x, nGroup) {
+#   cl <- makeCluster(detectCores())
+#   registerDoParallel(cl)
+#   n <- ncol(x) 
+#   if(!(n %in% c(2,3))) { # check if 2d or 3d
+#     stop("x must have either 2 or 3 columns")
+#   }
+#   
+#   fit <- hclust(dist(x), method="complete") # cluster, wont work on large datasets
+#   groups <- cutree(fit, k=nGroup)
+#   
+#   if(n == 3) { # 3d plot
+#     plot3d(x, col=groups, type="s", size=1, axes=F)
+#     axes3d(edges=c("x--", "y--", "z"), lwd=3, axes.len=2, labels=FALSE)
+#     grid3d("x")
+#     grid3d("y")
+#     grid3d("z")
+#   } else { # 2d plot
+#     maxes <- apply(abs(x), 2, max)
+#     rangeX <- c(-maxes[1], maxes[1])
+#     rangeY <- c(-maxes[2], maxes[2])
+#     plot(x, col=groups, pch=19, xlab=colnames(x)[1], ylab=colnames(x)[2], xlim=rangeX, ylim=rangeY)
+#     lines(c(0,0), rangeX*2)
+#     lines(rangeY*2, c(0,0))
+#   }
+#   stopCluster(cl)
 # }
